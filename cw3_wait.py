abb3dec15fd20cde0b6565c146f087e8fa23b86f
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-03-22 13:19:42 +0100

    explicitly wait

--- cw3_wait.py
+++ cw3_wait.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 # inicjalizacja przeglądarki Chrome
 driver = webdriver.Chrome()
 
 # implicitly wait - oczekiwanie na pojawie się każdego elementu na stronie przez maksymanie X (podajemy jako parametr) sekund
-driver.implicitly_wait(5)
+# driver.implicitly_wait(5)
 
 url = "https://www.w3schools.com/"
 
@@ -17,6 +19,7 @@ driver.get(url)
 driver.maximize_window()
 
 # ciasteczka
+time.sleep(5)
 accept_cookies2 = driver.find_element("id", "accept-choices")
 accept_cookies2.click()
 # driver.find_element("id", "accept-choices").click()
@@ -30,6 +33,18 @@ menu.click()
 learnHTML = driver.find_element('xpath', '//*[@id="tutorials_html_css_links_list"]/div[1]/a[1]')
 #learnHTML = driver.find_element('xpath', '//a[@title="HTML Tutorial"]')
 learnHTML.click()
+
+# explicitly wait - czekamy na konketny element
+wait = WebDriverWait(driver,10,0.5)
+# warunek oczekiwania
+wait.until(EC.visibility_of_element_located(('xpath', '//*[@id="leftmenuinnerinner"]/a[43]')))
+# gdyby był problem, to można uzyc starej metody
+# wait.until(EC.visibility_of_element_located((By.XPATH , '//*[@id="leftmenuinnerinner"]/a[43]')))
+# wait.until(lambda x: len(x.find_elements('xpath', '//*[@id="leftmenuinnerinner"]/a[43]')))
+
+# menu 'input types'
+menuInput = driver.find_element('xpath', '//*[@id="leftmenuinnerinner"]/a[43]')
+menuInput.click()
 
 #zatrzymanie skryptu
 time.sleep(500)
---
+++ lambda.py
@@ -0,0 +1,9 @@
+def funkcja(x):
+    return x*2
+
+
+print(funkcja(5))
+
+duble_number = lambda x: x*2
+
+print(duble_number(5))
