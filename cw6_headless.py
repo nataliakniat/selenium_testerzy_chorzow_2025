59512a7ca3e00249c6994721d113ec9f9116d661
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-03-23 13:03:23 +0100

    headless

--- cw6_headless.py
+++ cw6_headless.py
@@ -18,10 +18,26 @@ driver.get(url)
 # czekamy na tabele
 #/html/body/div[5]/section[1]/div/section/div[1]/div/table
 wait = WebDriverWait(driver, 10, 0.5)
-wait.until(EC.presence_of_all_elements_located((By.XPATH, '//table[contains(@class, "zebra")]')))
+wait.until(EC.presence_of_all_elements_located((By.XPATH, '//table[contains(@class, "zebra")]//tr')))
 
-rows = driver.find_elements(By.XPATH, '//table[contains(@class, "zebra")]')
+rows = driver.find_elements(By.XPATH, '//table[contains(@class, "zebra")]//tr')
 
-print(len(rows))
+data = []
+for row in rows[1:]:
+    cols = row.find_elements(By.TAG_NAME, 'td')
+
+    if len(cols) > 2:
+        city = cols[0].text
+        time = cols[1].text
+
+        print("Miasto: ", city, "Czas: ", time)
+        data.append([city, time])
 
 driver.quit()
+
+print("lista: ", data)
+
+df = pd.DataFrame(data, columns=['City', 'Time'])
+print("DataFrame: ", df)
+
+df.to_csv("dane_ze_strony.csv", index=False)
